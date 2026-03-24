f70badc8cdc1ff2c00476ea73184b20d50c20148
SisiSal <salamehselelina2002@gmail.com>
2026-03-24 16:55:11 -0400

    add advanced features

---
+++ DQN_CNN.py
@@ -0,0 +1,148 @@
+### Deep Q-Learing with a CNN feature extractor for Pacman
+import random
+import gymnasium as gym
+import pacman_gym
+
+from stable_baselines3 import DQN, PPO
+from feature_extractors import policy_kwargs
+from stable_baselines3.common.evaluation import evaluate_policy
+from stable_baselines3.common.monitor import Monitor
+
+######################################
+## Create the environment
+######################################
+train_maps1 = ["easy_01"]
+
+train_maps2 = ["easy_01", "medium_01", "medium_02",
+              "hard_01", "hard_02"]
+
+train_maps3 = ["easy_01",
+               "medium_01", "medium_02", "medium_03", "medium_04",
+              "hard_01", "hard_02", "hard_03", "hard_04"]
+
+test_maps  = ["easy_02",
+              "medium_05", "medium_06",
+              "hard_05", "hard_06"]
+
+def make_env(train_layouts, test_layouts, split, seed=0):
+    env = gym.make(
+        'gymnasium_env/PacmanGen-v0',
+        seed=seed, 
+        render_or_not=False, 
+        render_mode="tinygrid",
+        train_layouts=train_layouts, 
+        test_layouts=test_layouts, 
+        split=split,
+        max_steps=300
+    )
+    return Monitor(env)
+
+env1 = make_env(train_maps1, test_maps, split="train")
+env2 = make_env(train_maps2, test_maps, split="train")
+env3 = make_env(train_maps3, test_maps, split="train")
+
+######################################
+## Hyperparameter tuning
+######################################
+
+def sample_hyperparams_DQN():
+    return {
+        "learning_rate": 10 ** random.uniform(-5, -3),
+        "buffer_size": random.choice([50000, 100000]),
+        "learning_starts": random.choice([100, 1000, 5000]),
+        "train_freq": random.choice([1, 4, 8]),
+        "gradient_steps": random.choice([1, 4, 8]),
+        "target_update_interval": random.choice([100, 500, 1000]),
+        "gamma": random.choice([0.95, 0.99, 0.995]),
+        "exploration_fraction": random.choice([0.1, 0.2, 0.3]),
+        "exploration_final_eps": random.choice([0.01, 0.05, 0.1]),
+    }
+
+def run_trial(trial_id, total_timesteps=100_000):
+
+    params = sample_hyperparams_DQN()
+
+    model = DQN(
+        policy="CnnPolicy",
+        env=env1,
+        policy_kwargs=policy_kwargs,
+        verbose=0,
+        **params,
+    )
+
+    model.learn(total_timesteps=total_timesteps)
+
+    mean_reward, std_reward = evaluate_policy(
+        model,
+        env1,
+        n_eval_episodes=10,
+        deterministic=True,
+    )
+
+    env1.close()
+
+    return {
+        "trial_id": trial_id,
+        "params": params,
+        "mean_reward": mean_reward,
+        "std_reward": std_reward,
+    }
+
+results = []
+n_trials = 20
+
+
+for i in range(n_trials):
+    result = run_trial(i, total_timesteps=100_000)
+    results.append(result)
+    print(f"Trial {i}: mean_reward={result['mean_reward']:.2f}, params={result['params']}")
+
+best_result = max(results, key=lambda x: x["mean_reward"])
+print("\nBest trial:")
+print(best_result)
+
+
+######################################
+## Train the DQN agent
+######################################
+
+model = DQN('CnnPolicy', 
+            env1, 
+            policy_kwargs=policy_kwargs,
+            verbose=1,
+            tensorboard_log="./tensorboard_logs/")
+
+model.learn(total_timesteps=100000, #~3mil steps for ~25k episodes
+            log_interval=10,
+            tb_log_name="dqn_easy_01")
+
+model.save("dqn_easy_01")
+
+#del model
+
+#model = DQN.load("dqn_easy_01", env=env1)
+
+obs, info = env1.reset()
+print(f"Initial observation shape: {obs.shape}, observation space: {env1.observation_space}, info: {info}")
+for episode in range(5):
+    done = False
+    while not done:
+        action, _states = model.predict(obs, deterministic=True)
+        obs, reward, terminated, truncated, info = env1.step(action)
+        done = terminated or truncated
+    
+    print(f"Episode {episode + 1} finished info: {info}")
+    obs, info = env1.reset()
+
+env1.close
+
+######################################
+## Train the PPO agent
+######################################
+
+model = PPO('CnnPolicy', 
+            env1,
+            policy_kwargs=policy_kwargs,
+            verbose=1)
+model.learn(total_timesteps=10000, log_interval=4)
+model.save("ppo_pacman")
---
+++ feature_extractors.py
@@ -0,0 +1,225 @@
+### Custom feature extractors
+import torch as th
+import torch.nn as nn
+from gymnasium import spaces
+
+from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
+
+from pacman_gym.envs.pacman.game import Directions, Actions
+import pacman_gym.envs.pacman.util as utils
+########################################
+## Custom feature extractor for DQN
+########################################
+
+class SmallPacmanCNN(BaseFeaturesExtractor):
+    def __init__(self, observation_space: spaces.Box, features_dim: int = 256):
+        super().__init__(observation_space, features_dim)
+
+        n_input_channels = observation_space.shape[0] # number of channels
+
+        self.cnn = nn.Sequential(
+            nn.Conv2d(n_input_channels, 8, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(8, 16, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(16, 32, kernel_size=4, stride=1, padding=0),
+            nn.ReLU(),
+            nn.Flatten(),
+        )
+
+        with th.no_grad():
+            sample = th.as_tensor(observation_space.sample()[None]).float()
+            n_flatten = self.cnn(sample).shape[1]
+
+        self.linear = nn.Sequential(
+            nn.Linear(n_flatten, features_dim),
+            nn.ReLU(),
+        )
+
+    def forward(self, observations: th.Tensor) -> th.Tensor:
+        return self.linear(self.cnn(observations))
+
+policy_kwargs = dict(
+    features_extractor_class=SmallPacmanCNN,
+    features_extractor_kwargs=dict(features_dim=256),
+)
+
+#################################################
+## Feature extractor for Approximate Q-Learning
+#################################################
+
+class FeatureExtractor:
+    def getFeatures(self, state, action):
+        """
+          Returns a dict from features to counts
+        """
+        utils.raiseNotDefined()
+
+class IdentityExtractor(FeatureExtractor):
+    def getFeatures(self, state, action):
+        """
+        Returns a feature vector with one feature for each (state,action) pair.
+        """
+        feats = utils.Counter()
+        feats[(state,action)] = 1.0
+        return feats
+
+class CoordinateExtractor(FeatureExtractor):
+    def getFeatures(self, state, action):
+        """
+        Breaks the state down into x and y coordinates and creates features for each
+        """
+        feats = utils.Counter()
+        feats[state] = 1.0
+        feats['x=%d' % state[0]] = 1.0
+        feats['y=%d' % state[1]] = 1.0
+        feats['action=%s' % action] = 1.0
+        return feats
+    
+def closestFood(pos, food, walls):
+    """
+    Returns the distance to the closest food using BFS
+    """
+    fringe = [(pos[0], pos[1], 0)]
+    expanded = set()
+
+    while fringe:
+        pos_x, pos_y, dist = fringe.pop(0)
+        if (pos_x, pos_y) in expanded:
+            continue
+        expanded.add((pos_x, pos_y))
+        # if we find a food at this location then exit
+        if food[pos_x][pos_y]:
+            return dist
+        # otherwise spread out from the location to its neighbours
+        nbrs = Actions.getLegalNeighbors((pos_x, pos_y), walls)
+        for nbr_x, nbr_y in nbrs:
+            fringe.append((nbr_x, nbr_y, dist+1))
+    # no food found
+    return None
+
+def closestTarget(pos, targets, walls):
+    """
+    Returns the distance to the closest target using BFS
+    """
+    fringe = [(pos[0], pos[1], 0)]
+    expanded = set()
+    targets = set(targets)
+
+    while fringe:
+        pos_x, pos_y, dist = fringe.pop(0)
+        if (pos_x, pos_y) in expanded:
+            continue
+        expanded.add((pos_x, pos_y))
+        
+        # if we find a target at this location then exit
+        if (pos_x, pos_y) in targets:
+            return dist
+        
+        # otherwise spread out from the location to its neighbours
+        nbrs = Actions.getLegalNeighbors((pos_x, pos_y), walls)
+        for nbr_x, nbr_y in nbrs:
+            fringe.append((nbr_x, nbr_y, dist+1))
+
+    # no target found
+    return None
+
+class AdvancedExtractor(FeatureExtractor):
+    """
+    Returns simple features for a basic reflex Pacman:
+    - whether food will be eaten
+    - distance to the closest food
+    - whether a capsule will be eaten
+    - distance to the closest capsule
+    - whether a ghost collision is imminent
+    - whether an active ghost is one step away
+    - whether a scared ghost is one or two steps away
+    - distance to the closest active ghost
+    - inverse of the distance to the closest active ghost (as a measure of risk)
+    - distance to the closest scared ghost
+    """
+
+    def getFeatures(self, state, action):
+        # extract the grid of food and wall locations and get the ghost locations
+        food = state.getFood()
+        walls = state.getWalls()
+        capsules = state.getCapsules()
+        ghost_states = state.getGhostStates() # to get ghost positions and scared times
+
+        features = utils.Counter()
+
+        # Feature 1: bias, always 1.0 
+        # Acts as a bias term for the weights.
+        # This allows the agent to learn a baseline value 
+        # for taking an action in any state, 
+        # even if all other features are zero.
+        features["bias"] = 1.0
+
+        # current pacman position
+        x, y = state.getPacmanPosition()
+
+        # next pacman position after he takes the action
+        dx, dy = Actions.directionToVector(action)
+        next_x, next_y = int(x + dx), int(y + dy)
+
+        # Split the ghost states into scared and active ghosts
+        active_ghosts = []
+        scared_ghosts = []
+
+        # Get positions of active and scared ghosts
+        for ghost in ghost_states:
+            ghost_pos = ghost.getPosition()
+            if ghost_pos is not None:
+                ghost_pos = (int(ghost_pos[0]), int(ghost_pos[1]))
+                if ghost.scaredTimer > 0:
+                    scared_ghosts.append(ghost_pos)
+                else:
+                    active_ghosts.append(ghost_pos)
+
+        # Feature 2: number of active ghosts 1-step away
+        features["#-of-active-ghosts-1-step-away"] = sum(
+            (next_x, next_y) in Actions.getLegalNeighbors(g, walls) 
+            for g in active_ghosts
+            )
+
+        # Feature 3: number of scared ghosts 1-or-2-step away
+        scared_count = 0
+        for g in scared_ghosts:
+            d = closestTarget((next_x, next_y), [g], walls)
+            if d is not None and d <= 2:
+                scared_count += 1
+        features["#-of-scared-ghosts-1-or-2-step-away"] = scared_count
+
+        # Feature 4: if there is no danger of ghosts then add the food feature
+        if not features["#-of-active-ghosts-1-step-away"] and food[next_x][next_y]:
+            features["eats-food"] = 1.0
+
+        # Feature 5: distance to the closest food
+        dist_food = closestFood((next_x, next_y), food, walls)
+        if dist_food is not None:
+            # make the distance a number less than one otherwise the update
+            # will diverge wildly
+            features["closest-food"] = float(dist_food) / (walls.width * walls.height)
+        
+        # Feature 6: if there is danger of ghosts then add the capsule feature
+        if features["#-of-active-ghosts-1-step-away"] and (next_x, next_y) in capsules:
+            features["eats-capsule"] = 1.0
+
+        # Feature 7: distance to the closest capsule
+        dist_capsule = closestTarget((next_x, next_y), capsules, walls)
+        if dist_capsule is not None:
+            features["closest-capsule"] = float(dist_capsule) / (walls.width * walls.height)
+
+        # Feature 8 and 9: distance and inverse distance to the closest active ghost
+        dist_active_ghost = closestTarget((next_x, next_y), active_ghosts, walls)
+        if dist_active_ghost is not None:
+            features["closest-active-ghost"] = float(dist_active_ghost) / (walls.width * walls.height)
+            features["inv-closest-active-ghost"] = 1.0 / (1.0 + float(dist_active_ghost))
+
+        # Feature 10: distance to the closest scared ghost
+        dist_scared_ghost = closestTarget((next_x, next_y), scared_ghosts, walls)
+        if dist_scared_ghost is not None:
+            features["closest-scared-ghost"] = float(dist_scared_ghost) / (walls.width * walls.height)
+
+        features.divideAll(10.0)
+        return features
